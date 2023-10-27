c87bd0ec7936be6b9293a98df7d92a2d8b9eed77
Van Nguyen <van.nguyenthi@asnet.com.vn>
2023-10-27 22:56:31 +0700

    feat: use product types instead interfaces

--- product-management/src/services/api-action.ts
+++ product-management/src/services/api-action.ts
@@ -3,8 +3,8 @@ import axios from 'axios';
 // constants
 import { MESSAGES } from '@constants';
 
-// interfaces
-import { Product } from '@interfaces';
+// types
+import { Product } from '@types';
 
 // constants
 import { PRODUCTS_API } from '@constants';
