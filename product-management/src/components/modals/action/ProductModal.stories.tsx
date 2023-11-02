b98560a0d62ada46d4beb6d328f9390a0925e94f
Van Nguyen <van.nguyenthi@asnet.com.vn>
2023-11-02 17:20:09 +0700

    feat: update prop product data for case edit modal

--- product-management/src/components/modals/action/ProductModal.stories.tsx
+++ product-management/src/components/modals/action/ProductModal.stories.tsx
@@ -4,7 +4,7 @@ import Storybook from '@storybook/react';
 // Components
 import ProductModal from '.';
 
-import { MOCKED_PRODUCT_VALUE_DEFAULT } from '@constants';
+import { MOCKED_PRODUCT_VALUE_DEFAULT, MOCKED_PRODUCT_EXIST } from '@constants';
 
 // Code
 import { asEdit, asDefault } from './ProductModal.code';
@@ -43,7 +43,7 @@ Default.parameters = {
 
 export const Edit = Template.bind({});
 Edit.args = {
-  product: MOCKED_PRODUCT_VALUE_DEFAULT,
+  product: MOCKED_PRODUCT_EXIST,
   action: 'edit'
 };
 Edit.parameters = {
